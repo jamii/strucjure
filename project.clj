95135a8fcdf75c0f5dc50dec374583db448ca8e8
Jamie Brandon <jamie@scattered-thoughts.net>
2012-12-10 16:22:57 -0500

    Not actually using core.cache any more

--- project.clj
+++ project.clj
@@ -4,6 +4,4 @@
   :license {:name "LGPL"
             :url "http://www.gnu.org/licenses/lgpl.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [slingshot "0.10.3"]
-                 [org.clojure/core.cache "0.6.2"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.3"]]}})
+                 [slingshot "0.10.3"]])

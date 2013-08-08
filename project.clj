8686d2a34e8f3bdd1cd40689197707b1de818129
Jamie Brandon <jamie@scattered-thoughts.net>
2013-08-08 12:42:34 +0100

    Pull in prismatic/plumbing for the graph functions

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [prismatic/plumbing "0.1.0"]])

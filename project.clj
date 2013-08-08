8b561160011b41124f9082870e68a6ccfd0b61e7
Jamie Brandon <jamie@scattered-thoughts.net>
2013-08-08 12:41:32 +0100

    Relicense to EPL
    Most clojure projects default to using EPL, which may not be compatible with LGPL.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject strucjure "0.3.5"
+(defproject strucjure "0.4.0-SNAPSHOT"
   :description "Pattern-matching, parsing and generic traversals via PEGs"
   :url "http://github.com/jamii/strucjure"
-  :license {:name "LGPL"
-            :url "http://www.gnu.org/licenses/lgpl.html"}
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.5.1"]])

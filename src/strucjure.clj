e818336ab2d45f5f2db99795246ccd49855b1e0f
Jamie Brandon <jamie@scattered-thoughts.net>
2013-10-08 17:26:00 +0100

    Update todo list

--- src/strucjure.clj
+++ src/strucjure.clj
@@ -1,36 +1,15 @@
-(ns strucjure (:require [strucjure.pattern :as p]
-                       [strucjure.graph :as g]
-                       [strucjure.sugar :as s]
-                       [strucjure.view :as v]
-                       [plumbing.core :refer [fnk]]))
+(ns strucjure)
 
 ;; --- TODO ---
-;; finish ns example
 ;; README - http://hugoduncan.org/post/evaluate_clojure_in_emacs_markdown_buffers/ or similar
 ;; tests (string in regression, readme, bootstrap, generative)
-;; optimisations in view
-;; error reporting - deepest-error in graph, maybe first-error in pattern
-;; Input pattern for prewalks?
-;; license?
-
-;; --- SKETCH ---
-;; if not output? return input instead (and then look for Output in children)
-;; pseudo-patterns should use callback in view compiler
-;; could import graphs using nested names eg :require:libspec
-;; the problem with ns-grammer is not a bug - its a semantics problem...
-;;   could be fixed in this case with (or option libspec)
-;;   but is confusing and broken in general
-;;   at minimum need to be able to push context down into nodes
 
 ;; --- LATER ---
-;; for prewalks can just build ast by attaching name and bindings in metadata
-;; need to be able to alter views (store original pattern in meta and have pattern/alter and graph/alter)
 ;; cut by returning delay - can trampoline to the nearest try - needs work inside Or/ZeroOrMore
 ;; gens
 ;; types
 ;; string patterns, ~(chain "foo" "/" "bar")
 ;; binary patterns
-;; reimplement core
 
 ;; --- LESSONS ---
 ;; really need proper mutable vars for compiler target
@@ -48,51 +27,9 @@
 ;; getting binding right is hard. parsec et al solve this by just not doing it
 ;; write down thinking to avoid going in circles
 ;; benchmark as soon as possible
+;; without a specific goal ends up heavily over-engineered
 
 ;; --- MOTIVATION ---
 ;; types vs data
 ;; correct-by-construction as opposed to analysing ad-hoc code
 ;; use :refer-all as example
-
-(comment
-  (def ot (pattern (1 2)))
-  [0 * ~ot] --> [0 ((1 2) (1 2))]
-  [0 * & ~ot] --> [0 (1 2 1 2)]
-  [0 & * ~ot] --> [0 (1 2) (1 2)]
-  [0 & * & ~ot] --> [0 1 2 1 2])
-
-;; http://www.mercury.csse.unimelb.edu.au/information/papers/packrat.pdf
-
-;; 'This paper argues that (a) packrat parsers can be trivially implemented using a combination
-;; of deﬁnite clause grammar rules and memoing, and that (b) packrat
-;; parsing may actually be signiﬁcantly less eﬃcient than plain recursive
-;; descent with backtracking, but (c) memoing the recognizers of just one or
-;; two nonterminals, selected in accordance with Amdahl’s law, can some-
-;; times yield speedups. We present experimental evidence to support these
-;; claims.'
-
-(def ns-grammar
-  (s/graph
-   ns (ns ^name ~symbol & ? ~docstring & ? ~attr-map & * ~reference)
-   docstring ~(s/is string?)
-   attr-map ~(s/is map?)
-   reference ~(s/or ~require ~import)
-   require (:require & * ~libspec)
-   libspec ~(s/or ~symbol
-                  [^prefix ~symbol & * ~libspec]
-                  [~symbol & * & ~option])
-   option ~(s/or (:as ~symbol)
-                 (:refer ~(s/or :all [& * ~symbol]))
-                 (:reload)
-                 (:reload-all)
-                 (:verbose))
-   import (:import & * [~symbol & * ~symbol])
-   symbol ~(s/is symbol?)))
-
-(def ns-validate
-  (v/with-layers [v/with-depth v/with-deepest-failure]
-    (v/*view* (s/node-of 'ns ns-grammar))))
-
-(strucjure/ns-validate '(ns foo (:require [bar :refer :all])))
-
-(strucjure/ns-validate '(ns foo (:require [bar :refer-all])))

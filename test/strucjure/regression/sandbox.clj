cdfb9514318e4e128a13fd661123acc2389b23c6
Jamie Brandon <jamie@scattered-thoughts.net>
2013-12-05 12:32:55 +0000

    Remove hopelessly outdated tests

--- test/strucjure/regression/sandbox.clj
+++
@@ -1,9 +0,0 @@
-(ns strucjure.regression.sandbox
-  (:refer-clojure :exclude [with-meta or and * + ? num])
-  (:require [clojure.stacktrace :refer [e]]
-            [plumbing.core :refer [map-vals fnk]]
-            [strucjure.pattern :refer :all :exclude [with-binding]]
-            [strucjure.graph :refer :all :exclude [with-binding]]
-            ;;[strucjure.debug :refer :all]
-            [strucjure.view :refer :all :exclude [view]]
-            [strucjure.sugar :refer :all]))
